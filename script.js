95b3bd0c9c70db6a5656e65fe6ddae929f3b83f0
mittermann <83854785+mittermann@users.noreply.github.com>
2021-05-08 02:56:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
+  console.log(slides, slideIndex);
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
